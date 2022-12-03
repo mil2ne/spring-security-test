2bc7fae5e2d29a807475cad9037a8fa133ec9d27
celee <show3070@gmail.com>
2022-12-03 17:39:21 +0900

    #1 spring-security
    개인노트 테스트

---
+++ src/main/java/me/benny/practice/spring/security/SpringSecurityPracticeApplication.java
@@ -0,0 +1,17 @@
+package me.benny.practice.spring.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+/**
+ * @author benny.ahn
+ */
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+//@SpringBootApplication
+public class SpringSecurityPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityPracticeApplication.class, args);
+    }
+}
